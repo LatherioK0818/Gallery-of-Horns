ea6738ca2810df1075589e771d640572cb5793db
Latherio Kidd <kidd.latherio@gmail.comS>
2023-11-27 22:57:56 -0500

    Updates for Lab 1

---
+++ Gallery-of-Horns/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './footer.module.css';
+const Footer = () => {
+  return (
+    <footer>
+      <p>Author: Your Name</p>
+    </footer>
+  );
+};
+
+export default Footer;
